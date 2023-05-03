cbcd12cbbcb619f2427479b54fb1196f66687697
VadymMurzin <prometey736@gmail.com>
2023-05-03 16:51:39 +0200

    Add task 1-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const inputData = document.querySelector('.feedback-form');
+
+const saveEmailMessage = throttle(function (event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const formData = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  //   console.log(email.value, message.value);
+}, 500);
+
+inputData.addEventListener('input', saveEmailMessage);
+
+const form = document.querySelector('.feedback-form');
+const { email, message } = form.elements;
+
+const savedFormData = localStorage.getItem('feedback-form-state');
+if (savedFormData) {
+  const parsedFormData = JSON.parse(savedFormData);
+  email.value = parsedFormData.email;
+  message.value = parsedFormData.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = { email: email.value, message: message.value };
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  message.value = '';
+});
